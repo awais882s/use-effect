1f53e8c88cf57366c9481f33b158deecca6b156f
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-06-27 23:01:12 +0500

    reset btn

--- src/pages/Hooks/Hook.js
+++ src/pages/Hooks/Hook.js
@@ -1,22 +1,29 @@
 import React, { useState } from 'react'
 export default function Hook() {
     const [count, setCount] = useState(0)
-    return (
+    const decrementHandler = () => {
+        if (count > 0) {
+            setCount(count - 1)
+        }else{
+            setCount(0)
+        }
 
+    }
+    return (
         <div div className="container py-5" >
             <div className="row">
                 <div className="col">
                     <h1 className='text-center text-primary'>useState Hook</h1>
-
                 </div>
             </div>
             <div className="row mt-4 text-center">
                 <div className="col">
-                    <button className='btn btn-success'>Increment</button>
+                    <button className='btn btn-success' onClick={() => { setCount(count + 1) }}>Increment</button>
                     <h3 className='mb-4 my-4'>{count}</h3>
-                    <button className='btn btn-primary'>Decrement</button>
+                    <button className='btn btn-primary' onClick={() => {decrementHandler() }}>Decrement</button>
+                    <button className='btn btn-warning' onClick={() => { setCount(0) }}>Reset</button>
                 </div>
             </div>
         </div>
-    )
+    );
 }
