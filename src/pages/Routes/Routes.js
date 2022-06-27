9e67ee01c4687d0293c480fb7fe433dff6d4629b
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-06-27 14:31:01 +0500

    pages

--- src/pages/Routes/Routes.js
+++ src/pages/Routes/Routes.js
@@ -3,19 +3,26 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from "../Home/Home";
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
+// pages
+import About from '../About/About';
+import Contact from '../contact/Contact';
+import Login from '../Login/Login';
 export default function Routing() {
 
     return (
         <>
             <BrowserRouter>
                 <Header />
-                <main>
+                <ma in>
                     <Routes>
-                        <Home />
+                        <Route path='/' element={<Home />} />
+                        <Route path='login' element={<Login />} />
+                        <Route path='contact' element={<Contact />} />
+                        <Route path='ablout' element={<About />} />
 
 
                     </Routes>
-                </main>
+                </ma>
                 <Footer />
             </BrowserRouter>
 
