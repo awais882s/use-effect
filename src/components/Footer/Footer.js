9bd860c72d730198fb70b8e66557d986f67992ff
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-06-27 14:52:54 +0500

    scss

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
 export default function Footer() {
-    
+
   return (
-    <div>Footer</div>
-    
+    <>
+      <footer>
+        <p>&copy; 2022 <a href="https://muhammad-awais-house.business.site/">Muhammad Awais House</a>All right Reserved</p>
+      </footer>
+    </>
   )
 }
