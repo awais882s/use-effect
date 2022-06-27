eabcd9fc1b3a14399bee75567b7e80250ed517be
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-06-27 15:05:34 +0500

    Update Home.js

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,9 @@
 import React from 'react'
-import About from '../About/About'
-import Contact from '../contact/Contact'
 import Index from '../Login'
 export default function Home() {
 
     return (
         <>
-            <About />
-            <Contact />
             <Index />
         </>
 
