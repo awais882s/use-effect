ef66cd233ac0477db0ee5b27291e6929dc56fe74
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-06-27 12:40:34 +0500

    routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 function App() {
   return (
     <>
       <Header />
+      <main>
+
+      </main>
+      <Footer />
     </>
   );
 }
---
+++ src/pages/Routes/Routes.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
+export default function Routes() {
+
+    return (
+        <>
+            <Header />
+            <main>
+
+            </main>
+            <Footer />
+
+        </>
+
+
+    )
+}
