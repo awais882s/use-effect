d98385e282cdef2691f629d20a53e4b281b247a4
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-06-28 10:42:21 +0500

    reset btns

--- src/pages/Hooks/Hook.js
+++ src/pages/Hooks/Hook.js
@@ -6,18 +6,21 @@ export default function Hook() {
         count > 0 ? setCount(count - 1) : setCount(0)
     }
     return (
-        <div div className="container py-5" >
-            <div className="row">
-                <div className="col">
-                    <h1 className='text-center text-primary'>useState Hook</h1>
+        <div className='py-5'>
+            <div className="container" >
+                <div className="row">
+                    <div className="col">
+                        <h1 className='text-center text-primary'>useState Hook</h1>
+                    </div>
                 </div>
-            </div>
-            <div className="row mt-4 text-center">
-                <div className="col">
-                    <button className='btn btn-success' onClick={() => { setCount(count + 1) }}>Increment</button>
-                    <h3 className='mb-4 my-4'>{count}</h3>
-                    <button className='btn btn-primary' onClick={() => { decrementHandler() }}>Decrement</button>
+                <div className="row mt-4">
+                    <div className="col text-center">
+                        <button className='btn btn-success' onClick={() => { setCount(count + 1) }}>Increment</button>
+                        <h3 className='my-4'>{count}</h3>
+                        <button className='btn btn-primary' onClick={() => { decrementHandler() }}>Decrement</button><br />
+                        <button className='btn btn-warning mt-4' onClick={() => { setCount(0) }}>Reset </button>
 
+                    </div>
                 </div>
             </div>
         </div>
