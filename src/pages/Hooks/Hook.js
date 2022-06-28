3553b171623507f2736b44659ca408e8a8679b72
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-06-28 10:29:43 +0500

    condition as decrement

--- src/pages/Hooks/Hook.js
+++ src/pages/Hooks/Hook.js
@@ -2,11 +2,8 @@ import React, { useState } from 'react'
 export default function Hook() {
     const [count, setCount] = useState(0)
     const decrementHandler = () => {
-        if (count > 0) {
-            setCount(count - 1)
-        }else{
-            setCount(0)
-        }
+        setCount(count - 1);
+        count > 0 ? setCount(count - 1) : setCount(0)
     }
     return (
         <div div className="container py-5" >
@@ -19,8 +16,8 @@ export default function Hook() {
                 <div className="col">
                     <button className='btn btn-success' onClick={() => { setCount(count + 1) }}>Increment</button>
                     <h3 className='mb-4 my-4'>{count}</h3>
-                    <button className='btn btn-primary' onClick={() => {decrementHandler() }}>Decrement</button>
-                    <button className='btn btn-warning' onClick={() => { setCount(0) }}>Reset</button>
+                    <button className='btn btn-primary' onClick={() => { decrementHandler() }}>Decrement</button>
+
                 </div>
             </div>
         </div>
